a0e5bc9f85f338dfc5cecbfe2ef1535e2c890063
Vinh Nguyen <vinhnguyendinhbg@gmail.com>
2017-07-29 15:06:49 +0700

    comment controller

---
+++ controllers/CommentController.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const Router = express.Router();
+
+const CommentModel = require('../models/Comment.js');
+const IdeaModel = require('../models/Idea.js');
+
+// POST
+Router.post('/', (req, res) => {
+  Promise.all([userPromise, ideaPromise])
+  .then(
+      docs => {
+          return CommentModel.create({author: docs[0], post: docs[1]});
+      },
+      err => {
+          console.log(err);
+      }
+  )
+  .then(
+      comment => {
+          console.log(`SUCCESS addComment`);
+      },
+      err => {
+          console.log(`FAILED addComment`, err);
+      }
+  );
+});
+
+// GET: Get comment with id
+Router.get('/:id', (req, res) => {
+
+});
+
+// PUT: Update comment with id
+Router.put('/:id', (req, res) => {
+
+});
+
+// DELETE: Delete comment with id
+Router.delete('/:id', (req, res) => {
+
+});
