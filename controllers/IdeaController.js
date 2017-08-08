777ac09940a12ebc647f4fe5fcce2afe6a87b372
Vinh Nguyen <vinhnguyendinhbg@gmail.com>
2017-08-08 21:36:51 +0700

    add post, get, update and erase idea controller

--- controllers/IdeaController.js
+++ controllers/IdeaController.js
@@ -4,19 +4,35 @@ const Router = express.Router();
 const IdeaModel = require('../models/Idea');
 
 Router.post('/', (req, res) => {
-
+  IdeaModel.create(req.body).then(idea => {
+    res.send('Created idea');
+  }, err => {
+    res.send('Error create idea !!!');
+  });
 });
 
 Router.get('/:id', (req, res) => {
-
+  IdeaModel.get(req.params.id).then(idea => {
+    res.send(idea);
+  }, err => {
+    res.send('Error get idea !!!');
+  });
 });
 
 Router.put('/:id', (req, res) => {
-
+  IdeaModel.update(req.params.id, req.body).then(idea => {
+    res.send('Update idea');
+  }, err => {
+    res.send('Error update idea !!!');
+  });
 });
 
 Router.delete('/:id', (req, res) => {
-
+  IdeaModel.erase(req.params.id).then(idea => {
+    res.send('Delete idea successfully');
+  }, err => {
+    res.send('Error delete idea !!!');
+  });
 });
 
 module.exports = Router;
