509a6f7454d89b88c8655288c06c0d17f4298f36
Vinh Nguyen <vinhnguyendinhbg@gmail.com>
2017-08-26 19:25:38 +0700

    update css

--- DumbIdea React/src/views/AddNewIdea.js
+++ DumbIdea React/src/views/AddNewIdea.js
@@ -4,29 +4,29 @@ class AddNewIdea extends Component {
   render() {
     return (
       <div className="container add-new-idea">
-            <form method="post" >
-              <div>
-                <p className="add-new-idea-title">Thêm mới ý tưởng</p>
-                <hr />
-              </div>
-              <div>
-                <div>
-                  <p className="label-add-idea">Tên</p>
-                  <textarea name="name" id="nameIdea" className="form-control" rows="2" maxlength="200" placeholder="Như “Nơi nên đi” hoặc “Món ăn nên làm.”"></textarea>
-                </div>
-                <hr />
-                <div>
-                  <p className="label-add-idea">Mô tả</p>
-                  <textarea name="name" id="nameIdea" className="form-control" rows="4" maxlength="200" placeholder="Nội dung của bạn là gì?"></textarea>
-                  <hr />
-                </div>
-                <div>
-                  <p className="label-add-idea">Danh mục</p>
+        <div>
+          <p className="add-new-idea-title">Thêm mới ý tưởng</p>
+          <hr />
+        </div>
+        <form method="post" >
+          <div>
+            <div>
+              <p className="label-add-idea">Tên</p>
+              <textarea name="name" id="nameIdea" className="form-control" rows="2" maxlength="200" placeholder="Như “Nơi nên đi” hoặc “Món ăn nên làm.”"></textarea>
+            </div>
+            <hr />
+            <div>
+              <p className="label-add-idea">Mô tả</p>
+              <textarea name="name" id="nameIdea" className="form-control" rows="4" maxlength="200" placeholder="Nội dung của bạn là gì?"></textarea>
+              <hr />
+            </div>
+            <div>
+              <p className="label-add-idea">Danh mục</p>
 
-                </div>
-                <hr />
-              </div>
-            </form>
+            </div>
+            <hr />
+          </div>
+        </form>
       </div>
 
     );
