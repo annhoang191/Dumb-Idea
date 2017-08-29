6687101ed4b8953ef4b6a15c4e23bf381cd4fd81
AnnHoang <hquynhanh19@gmail.com>
2017-08-29 19:49:27 +0700

    styling login page

---
+++ DumbIdea React/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Header from './components/Header';
+import MainNav from './components/MainNav';
+import Home from './views/Home';
+import Footer from './components/Footer';
+import Page404 from './views/Page404'
+import About from './views/About';
+import TopIdea from './views/TopIdea';
+import All from './views/All';
+import AddNewIdea from './views/AddNewIdea';
+import Login from './views/Login';
+import Register from './views/Register';
+
+class App extends Component {
+  render() {
+    const routes = [
+      {
+        path      : '/',
+        exact     : true,
+        component : Home
+      },
+      {
+        path      : '/about',
+        exact     : true,
+        component : About
+      },
+      {
+        path      :'/topidea',
+        exact     : true,
+        component : TopIdea
+      },
+      {
+        path      :'/all',
+        exact     : true,
+        component : All
+      },
+      {
+        path      : '/addnewidea',
+        exact     : true,
+        component : AddNewIdea
+      },
+      {
+        path      : '/login',
+        exact     : true,
+        component : Login
+      },
+      {
+        path      : '/register',
+        exact     : true,
+        component : Register
+      },
+      {
+        path      : '*',
+        component : Page404
+      }
+    ];
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <div>
+            <Switch>
+              {routes.map((route,index) =>(
+                <Route key={index} {...route} />
+              ))}
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
